34f32e7c1718dad2ed540076c07967eee16aff16
desouvik <isouvikde@hotmail.com>
2024-01-02 15:42:55 -0500

    Initial Check in With static Data and CRUD Operation.

---
+++ EmployeeRegistrationService/Controllers/EmployeeController.cs
@@ -0,0 +1,67 @@
+﻿using EmployeeRegistrationService.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeRegistrationService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private static readonly List<Employee> _employees = new List<Employee>() {
+            new Employee { Id = 1, FirstName="Peter", LastName="Parker", Email="spiderman@avengers.com", City="New York"},
+            new Employee { Id = 2, FirstName = "Bruce", LastName = "Wayne", Email = "batman@avengers.com", City = "New York" },
+            new Employee { Id = 3, FirstName = "Tony", LastName = "Stark", Email = "ironman@avengers.com", City = "New York" },
+            new Employee { Id = 4, FirstName = "Thor", LastName = "Odinson", Email = "thor@avengers.com", City = "New York" }
+            };
+
+        public EmployeeController() { }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Employee>>> GetEmployees()
+        {
+            return Ok(_employees);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetSingleEmployee(int id)
+        {
+            var employee = _employees.Find(x => x.Id == id);
+            if (employee == null)
+                return NotFound("Employee is not found!");
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Employee>>> CreateEmployee([FromBody]Employee employee)
+        {
+            _employees.Add(employee);
+            return Ok(_employees);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Employee>>> DeleteEmployee(int id)
+        {
+            var employee = _employees.Find(x=> x.Id == id);
+            if (employee == null)
+                return NotFound("Employee is not found!");
+            _employees.Remove(employee);
+            return Ok(_employees);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<List<Employee>>> UpdateEmployee(int id, [FromBody]Employee request)
+        {
+            var employee = _employees.Find(x=>x.Id == id);
+            if (employee == null)
+                return NotFound("Employee is not found!");
+            employee.FirstName=request.FirstName;
+            employee.LastName=request.LastName;
+            employee.Email=request.Email;
+            employee.City=request.City;
+
+            return Ok(_employees);
+        }
+    }
+}
---
+++ EmployeeRegistrationService/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeRegistrationService.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+
+    }
+}
